e9f2fe5b59a514cc2bac668637ea871476ab7892
Brayan Frank <brayanfrank.sm@gmail.com>
2022-11-07 12:09:53 -0300

    corrections

--- python/2167.py
+++ python/2167.py
@@ -4,7 +4,7 @@ values = input().split()
 index = 0
 
 for i in range(n - 1):
-    if values[i] > values[i + 1]:
+    if int(values[i]) > int(values[i + 1]):
         index = i + 2
         break
     
