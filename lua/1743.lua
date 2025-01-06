c266e0dfbdac5682dfc792d5fd093d76d9e0d1c5
BrauUu <brayanfrank.sm@gmail.com>
2025-01-06 10:51:33 -0300

    06/01/25

--- lua/1743.lua
+++ lua/1743.lua
@@ -1,15 +1,15 @@
 local conector1 = io.read()
 local conector2 = io.read()
-local flag = false
-for i=1,10,2 do
+local notCompatible = false
+for i=1,9,2 do
     if tonumber(string.sub(conector1, i, i)) == tonumber(string.sub(conector2, i, i)) then
-        flag = true
+        notCompatible = true
         break
     end
 end
 
-if flag then
-    print('Y')
-else
+if notCompatible then
     print('N')
+else
+    print('Y')
 end
